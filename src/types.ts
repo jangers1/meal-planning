4dadd36517daf2ec4fac7d5140062fde359058f7
Roshan Jhangiani <jangers.rj@gmail.com>
2025-09-24 17:20:09 +0100

    Added standard Joy sizes and variants

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,3 @@
 export type JoyColours = 'primary' | 'neutral' | 'danger' | 'success' | 'warning';
+export type JoySizes = 'sm' | 'md' | 'lg';
+export type JoyVariants = 'solid' | 'soft' | 'outlined' | 'plain';
