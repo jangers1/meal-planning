36359c707c2b389858cf4d701dcee6c3aa11f95f
Roshan Jhangiani <jangers.rj@gmail.com>
2025-09-28 20:11:15 +0100

    Margin change

--- src/components/recipes/recipe_create/RecipeCreate.tsx
+++ src/components/recipes/recipe_create/RecipeCreate.tsx
@@ -15,8 +15,8 @@ function RecipeCreate() {
                 spacing={4}
                 sx={{
                     display: "flex",
-                    m: 2,
-                    flex: 1
+                    flex: 1,
+                    mt: 2
                 }}
             >
                 <RecipeHeaderInput/>
