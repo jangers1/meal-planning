8324bf4aa011ffe14c6e3d40581000f60a7ff682
Roshan Jhangiani <jangers.rj@gmail.com>
2025-10-04 11:17:24 +0100

    Created MealPlan page with tabs

--- src/pages/MealPlan.tsx
+++ src/pages/MealPlan.tsx
@@ -1,12 +1,33 @@
-import {Sheet, Typography} from '@mui/joy';
+import {Sheet, Tab, TabList, TabPanel, Tabs} from '@mui/joy';
+import WeekPlan from "../components/ui_components/WeekPlan.tsx";
 
 function MealPlan() {
-  return (
-    <Sheet sx={{p:2, width: '100%'}}>
-      <Typography level="h3">Meal Plan</Typography>
-      <Typography level="body-md" sx={{mt:1}}>Plan your meals for the week here.</Typography>
-    </Sheet>
-  );
+
+    return (
+        <Sheet
+            sx={{
+                width: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+            }}
+        >
+            <Tabs
+                size={'lg'}
+            >
+                <TabList>
+                    <Tab color={'neutral'}>Week Plan</Tab>
+                    <Tab color={'neutral'}>Month Overview</Tab>
+                </TabList>
+                <TabPanel value={0} sx={{p: 0}}>
+                    <WeekPlan/>
+                </TabPanel>
+                <TabPanel value={1}>
+
+                </TabPanel>
+            </Tabs>
+        </Sheet>
+    );
 }
 
 export default MealPlan;
