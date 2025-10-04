b4c4fedefe9aa02cfd5af3f5ef07fe764fe8de58
Roshan Jhangiani <jangers.rj@gmail.com>
2025-10-04 18:16:20 +0100

    Added tooltip positionings as a type

--- src/shared/types/ui.types.ts
+++ src/shared/types/ui.types.ts
@@ -1,3 +1,4 @@
 export type JoyColours = 'primary' | 'neutral' | 'danger' | 'success' | 'warning';
 export type JoySizes = 'sm' | 'md' | 'lg';
 export type JoyVariants = 'solid' | 'soft' | 'outlined' | 'plain';
+export type JoyTooltipPositions = 'top' | 'bottom' | 'left' | 'right' | 'top-start' | 'top-end' | 'bottom-start' | 'bottom-end' | 'left-start' | 'left-end' | 'right-start' | 'right-end';
